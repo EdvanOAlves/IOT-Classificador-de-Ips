0064da5a3bda87dc710c9bbe2970d350360484e6
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-05-06 09:43:23 -0300

    Interface em texto desativada

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    	Menu menu = new Menu();
+    	//INTERFACE EM TEXTO (Para troubleshooting)
+//    	Menu menu = new Menu();
+//    	menu.mostrarMenu();
+//    	Scanner scanner = new Scanner(System.in);
+//    	
+//    	System.out.println("Hello world!");
+//    	System.out.println("Olá calculadora humana!");
+//    	System.out.println("Esse é o classificador de ips, basta inserir um ip com máscara em formato CIDR");
+//    	System.out.println("Ex: 192.168.0.0/24");
+//    	String ip = scanner.nextLine();
+//    	Rede rede = new Rede(ip);
+    	
+    	//INTERFACE GRÁFICA
     	ConversorTela tela = new ConversorTela();
     	tela.criarTela("Classificador de Ips");
-    	menu.mostrarMenu();
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello world!");
-        System.out.println("Olá calculadora humana!");
-        System.out.println("Esse é o classificador de ips, basta inserir um ip com máscara em formato CIDR");
-        System.out.println("Ex: 192.168.0.0/24");
-
-        String ip = scanner.nextLine();
-
-        Rede rede = new Rede(ip);
 
 
 
